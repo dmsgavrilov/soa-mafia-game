7d7ac0bc5a2f65fcf2aae471624c67b2f7de91f6
Dmitrii <dima.gavrilov12let@gmail.com>
2022-03-19 11:41:01 +0300

    Cut off some commands

--- commands.py
+++ commands.py
@@ -1,16 +1,10 @@
 class ClientCommands:
     LEAVE = "/leave"
     MEMBERS = "/members"
-    CREATE_ROOM = "/create_room"
-    CONNECT_TO_ROOM = "/connect_to_room"
-    SET_SIZE = "/set_size"
-    START_GAME = "/start_game"
-    ROOMS = "/rooms"
     VERIFY = "/verify"  # For cherif
     KILL = "/kill"  # For cherif and mafia
     EXECUTE = "/execute"  # For citizen
     SKIP = "/skip"  # For citizen
-    PLAYERS = "/players"
     SELF = "/me"
     HELP = "/help"
     READY = "/ready"
